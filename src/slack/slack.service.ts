808e426384ad3678243709119490598007c199c9
Gašper Dobrovoljc <gasper.dobrovoljc1@gmail.com>
2021-09-17 23:21:50 +0200

    Discord fix

--- src/slack/slack.service.ts
+++ src/slack/slack.service.ts
@@ -1,24 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { WebClient } from '@slack/web-api';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class SlackService {
+  private client: WebClient;
+  private logger: Logger = new Logger('SlackService');
+
   constructor(private readonly prisma: PrismaService) {
     this.client = new WebClient(process.env.SLACK_TOKEN);
   }
 
-  client: WebClient;
-
   async send(message: string) {
-    await this.client.chat.postMessage({
-      channel: process.env.SLACK_CHANNEL,
-      text: message,
-    });
+    try {
+      await this.client.chat.postMessage({
+        channel: process.env.SLACK_CHANNEL,
+        text: message,
+      });
+    } catch (e) {
+      this.logger.error(e);
+    }
   }
 
   async reportUserCount() {
     const userCount = await this.prisma.user.count();
-    this.send(`There are ${userCount} users registered.`);
+    try {
+      await this.send(`There are ${userCount} users registered.`);
+    } catch (e) {
+      this.logger.error(e);
+    }
   }
 }
