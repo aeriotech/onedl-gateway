eaa9eaa0279af42627972e3629e23690855fc26c
Gašper Dobrovoljc <gasper.dobrovoljc1@gmail.com>
2021-10-06 20:44:17 +0200

    UpdateProfileDto Input Type

--- src/profile/dto/update-profile.dto.ts
+++ src/profile/dto/update-profile.dto.ts
@@ -1,12 +1,17 @@
+import { Field, InputType } from '@nestjs/graphql';
 import { IsString } from 'class-validator';
 
+@InputType()
 export class UpdateProfileDto {
+  @Field()
   @IsString()
   firstName: string;
 
+  @Field()
   @IsString()
   lastName: string;
 
+  @Field()
   @IsString()
   bio: string;
 }
