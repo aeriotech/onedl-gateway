00f7b682aeae69587562537fcb574abb29b923d9
Gašper Dobrovoljc <gasper.dobrovoljc1@gmail.com>
2021-11-03 13:08:12 +0100

    Fixed graphql errors

--- src/discount/dto/create-discount.dto.ts
+++ src/discount/dto/create-discount.dto.ts
@@ -1,22 +1,29 @@
+import { Field, InputType } from '@nestjs/graphql';
 import { IsInt, IsNotEmpty, IsString, Min } from 'class-validator';
 
+@InputType()
 export class CreateDiscountDto {
   @IsNotEmpty()
   @IsString()
+  @Field()
   name: string;
 
   @IsNotEmpty()
   @IsString()
+  @Field()
   description: string;
 
   @IsNotEmpty()
+  @Field()
   shopId: number;
 
   @IsInt()
   @Min(1)
+  @Field({ nullable: true })
   max?: number;
 
   @IsInt()
   @Min(1)
+  @Field({ nullable: true })
   parts?: number;
 }
--- src/discount/dto/update-discount.dto.ts
+++ src/discount/dto/update-discount.dto.ts
@@ -1,12 +1,17 @@
+import { Field, InputType } from '@nestjs/graphql';
 import { IsBoolean, IsEmpty, IsNotEmpty, IsString } from 'class-validator';
 
+@InputType()
 export class UpdateDiscountDto {
   @IsString()
+  @Field({ nullable: true })
   name?: string;
 
   @IsString()
+  @Field({ nullable: true })
   description?: string;
 
   @IsBoolean()
+  @Field({ nullable: true })
   public?: boolean;
 }
